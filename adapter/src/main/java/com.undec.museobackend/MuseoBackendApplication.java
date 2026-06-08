b7cdc4f5c65ec507f3e5663e7d760992ec252799
Tu nombre <anamariabmaza@gmail.com>
2026-06-08 18:47:19 -0300

    chore: estructura inicial del proyecto

---
+++ adapter/src/main/java/com.undec.museobackend/MuseoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.undec.museobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseoBackendApplication.class, args);
+    }
+
+}
